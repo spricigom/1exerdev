1dd921a7a7ae5a23bc2e8350c431c4d104e4a939
spricigom <emilispricigo.ifc@gmail.com>
2024-02-29 20:44:09 -0300

    Aula 1 - exercicios 1 e 2 feitos

---
+++ DevWebII/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+const contador = ref (0)
+function decrementar(){
+  if (contador.value > 0){
+    contador.value--
+  }
+}
+
+function reiniciar(){
+  contador.value = 0;
+}
+
+</script>
+
+<template>
+  <button @click="contador++">Adicionar</button>
+  <button @click="decrementar">Subtrair</button>
+  <button @click="reiniciar">Reiniciar</button>
+  <p>valor do contador : {{ contador }}</p>
+</template>
+
+<style scoped>
+  button{
+    background-color: palevioletred;
+    color: black;
+    font-weight: bolder;
+    border-radius: 2px;
+    margin: 3px;
+    padding: 20px;
+  }
+</style>
